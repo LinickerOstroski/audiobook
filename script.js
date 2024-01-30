138cceab776dc4701b68e95aa02270a6a5c22f79
Linicker <olinicker@gmail.com>
2024-01-30 18:13:51 -0300

    adicionando funcionalidade próximo

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const botaoPlayPause = document.getElementById('play-pause');
 const audioCapitulo = document.getElementById('audio-capitulo');
+const proximoCapitulo = document.getElementById("proximo")
 const numeroCapitulos = 10;
 
 let tocando = false;
+let capituloAtual = 1;
 
 function tocarFaixa(){
     audioCapitulo.play();
-
+    botaoPlayPause.classList.remove('bi-play-circle-fill')
+    botaoPlayPause.classList.add('bi-pause-circle-fill')
 }
 function pausarFaixa(){
     audioCapitulo.pause();
+    botaoPlayPause.classList.add('bi-play-circle-fill')
+    botaoPlayPause.classList.remove('bi-pause-circle-fill')
 }
 
 function tocarOuPausar(){
@@ -22,7 +27,24 @@ function tocarOuPausar(){
     }
 }
 
+function proximaFaixa(){
+    capituloAtual = capituloAtual + 1;
+    if(capituloAtual === numeroCapitulos){
+        capituloAtual = 1;
+    }else{
+        capituloAtual = capituloAtual + 1;
+    }
+    audioCapitulo.src = './src/books/dom-casmurro/' + capituloAtual + '.mp3';
+}
+
+function faixaAnterior(){
+    capituloAtual = capituloAtual - 1;
+
+}
+
 botaoPlayPause.addEventListener('click', tocarOuPausar)
+
+proximoCapitulo.addEventListener('click', proximaFaixa)
 
 
 
