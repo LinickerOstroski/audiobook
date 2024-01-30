90b77b93952f72ff39d228cc20504f8e29609140
Linicker <olinicker@gmail.com>
2024-01-30 16:30:28 -0300

    integrando js, botão funcional

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 const botaoPlayPause = document.getElementById('play-pause');
-
-
+const audioCapitulo = document.getElementById('audio-capitulo');
 const numeroCapitulos = 10;
+
+let tocando = false;
+
+function tocarFaixa(){
+    audioCapitulo.play();
+
+}
+function pausarFaixa(){
+    audioCapitulo.pause();
+}
+
+function tocarOuPausar(){
+    if(tocando == false){
+        tocando = true;
+        tocarFaixa();
+    }else{
+        tocando = false;
+        pausarFaixa();
+    }
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar)
+
+
+
+
